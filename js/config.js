732e90e55a39c7f0aa9eac2853670da68b741c99
taupikm <mtmciparasi@gmail.com>
2026-09-07 21:41:02 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // config.js - Konfigurasi API
 var APP_CONFIG = {
     // Ganti dengan URL Web App dari Google Apps Script
-    API_URL: 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbzefr6NITS1Knk_kM9m0VOGYSgRBtqtSY6zWdREha0mXBlww3LAQOmWkTvkbycUIfqicA/exec',
     
     // Konfigurasi MathJax
     MATHJAX_CONFIG: {
